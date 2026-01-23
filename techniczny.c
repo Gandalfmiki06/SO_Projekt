c941e9a7fbd6ce329daa4851217ebe24e64012b8
Mikołaj <163037224+Gandalfmiki06@users.noreply.github.com>
2026-01-23 23:25:31 +0100

    Refactor techniczny function for cleaner code

--- techniczny.c
+++ techniczny.c
@@ -1,12 +1,10 @@
 #include "hala.h"
 #include <unistd.h>
 
-void* techniczny(void* arg) {
-    int sektor = *(int*)arg;
-
-    while (!ewakuacja) {
+void* techniczny(void* arg)
+{
+    while (!ewakuacja)
         sleep(1);
-    }
 
     loguj("Techniczny: sektor pusty");
     return NULL;
