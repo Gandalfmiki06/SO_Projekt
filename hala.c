3f61481c360f4c9e7ca2491f742ad0c5bb1cf2e2
Mikołaj <163037224+Gandalfmiki06@users.noreply.github.com>
2026-01-23 23:23:41 +0100

    Refactor hala.c to improve structure and logging

--- hala.c
+++ hala.c
@@ -1,7 +1,7 @@
 #include "hala.h"
+#include <stdio.h>
+#include <unistd.h>
 #include <time.h>
-#include <unistd.h>     // getpid()
-#include <pthread.h>
 
 /* ===== PARAMETRY ===== */
 
@@ -17,10 +17,7 @@ pthread_mutex_t mutex_kasy = PTHREAD_MUTEX_INITIALIZER;
 /* ===== KOLEJKA ===== */
 
 Kibic* kolejka[MAX_KOLEJKA];
-int q_start = 0;
-int q_end = 0;
-int q_size = 0;
-int kibice_w_kolejce = 0;
+int q_start = 0, q_end = 0, q_size = 0, kibice_w_kolejce = 0;
 
 pthread_mutex_t mutex_kolejka = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_kolejka = PTHREAD_COND_INITIALIZER;
@@ -29,37 +26,33 @@ pthread_cond_t cond_kolejka = PTHREAD_COND_INITIALIZER;
 
 pthread_mutex_t mutex_bilety = PTHREAD_MUTEX_INITIALIZER;
 
+/* ===== WEJŚCIA ===== */
+
+sem_t kontrola[SEKTORY][2];
+pthread_mutex_t mutex_wejsc = PTHREAD_MUTEX_INITIALIZER;
+
+/* ===== KIEROWNIK ===== */
+
+int stop_wejsc = 0;
+int ewakuacja = 0;
+pthread_cond_t cond_wejsc = PTHREAD_COND_INITIALIZER;
+
 /* ===== LOG ===== */
 
 pthread_mutex_t mutex_log = PTHREAD_MUTEX_INITIALIZER;
 
-/* ===== FUNKCJA LOGUJĄCA Z PID ===== */
-
 void loguj(const char* tekst)
 {
     pthread_mutex_lock(&mutex_log);
-
     FILE* f = fopen("raport.txt", "a");
-    if (f)
-    {
-        fprintf(
-            f,
-            "[czas=%ld] [procPID=%d] [watekPID=%lu] %s\n",
+    if (f) {
+        fprintf(f,
+            "[%ld] [PID=%d] [TID=%lu] %s\n",
             time(NULL),
             getpid(),
             (unsigned long)pthread_self(),
-            tekst
-        );
+            tekst);
         fclose(f);
     }
-
     pthread_mutex_unlock(&mutex_log);
 }
-
-/* ===== WEJŚCIA / EWAKUACJA ===== */
-
-int stop_wejsc = 0;
-int ewakuacja = 0;
-
-pthread_mutex_t mutex_wejsc = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_wejsc = PTHREAD_COND_INITIALIZER;
