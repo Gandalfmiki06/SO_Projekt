f5dc6e68d63725fdb1c1cb3390de952aeb0cdc6c
Mikołaj <163037224+Gandalfmiki06@users.noreply.github.com>
2026-01-23 23:25:04 +0100

    Refactor kierownik function with reduced sleep times

--- kierownik.c
+++ kierownik.c
@@ -1,22 +1,20 @@
 #include "hala.h"
 #include <unistd.h>
 
-void* kierownik(void* arg) {
-    sleep(5);
-    pthread_mutex_lock(&mutex_wejsc);
+void* kierownik(void* arg)
+{
+    sleep(2);
     stop_wejsc = 1;
-    loguj("Kierownik: wstrzymanie wejœæ");
-    pthread_mutex_unlock(&mutex_wejsc);
+    loguj("Kierownik: stop wejsc");
 
-    sleep(5);
-    pthread_mutex_lock(&mutex_wejsc);
+    sleep(2);
     stop_wejsc = 0;
     pthread_cond_broadcast(&cond_wejsc);
-    loguj("Kierownik: wznowienie wejœæ");
-    pthread_mutex_unlock(&mutex_wejsc);
+    loguj("Kierownik: wznowienie wejsc");
 
-    sleep(10);
+    sleep(5);
     ewakuacja = 1;
     loguj("Kierownik: ewakuacja");
+
     return NULL;
 }
