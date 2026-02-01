c8a6c1a6914e8723eb1ca7cd11edfc388a9d2be9
Mikołaj <163037224+Gandalfmiki06@users.noreply.github.com>
2026-02-01 15:59:14 +0100

    Refactor main function for simulation and testing

--- main.c
+++ main.c
@@ -1,51 +1,75 @@
 #include "hala.h"
-#include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
-#include "hala.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>   // usleep
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+void menu_testow();
+void handler(int sig)
+{
+    przerwanie=1;
+    ewakuacja=1;
+    loguj("SYGNAL: wymuszenie ewakuacji");
+    pthread_cond_broadcast(&cond_wejsc);
+    pthread_cond_broadcast(&cond_kolejka);
+}
 
 int main()
 {
-    for (int i = 0; i < SEKTORY; i++) {
-        miejsca[i] = K / SEKTORY;
-        for (int j = 0; j < 2; j++)
-            sem_init(&kontrola[i][j], 0, 3);
+	int tryb;
+	printf("1 - Symulacja\n2 - Testy jednostkowe\nWybor: ");
+	scanf("%d",&tryb);
+
+	if(tryb == 2) {
+		menu_testow();
+    return 0;
+}
+	
+	
+	
+    signal(SIGINT, handler);
+    signal(SIGTERM, handler);
+    signal(SIGQUIT, handler);
+
+    srand(time(NULL));
+
+    for(int i=0;i<SEKTORY;i++){
+        miejsca[i]=K/SEKTORY;
+        for(int j=0;j<2;j++){
+            sem_init(&kontrola[i][j],0,3);
+            stanowisko_druzyna[i][j]=-1;
+        }
     }
 
-    pthread_t kasy[MAX_KASY], kibice[50], t_kier, t_tech;
+    pthread_t kasy[MAX_KASY], kibice[120], t_kier, t_tech;
 
-    pthread_create(&t_kier, NULL, kierownik, NULL);
-    pthread_create(&t_tech, NULL, techniczny, NULL);
+    pthread_create(&t_kier,NULL,kierownik,NULL);
+    pthread_create(&t_tech,NULL,techniczny,NULL);
 
-    for (int i = 0; i < MAX_KASY; i++)
-        pthread_create(&kasy[i], NULL, kasa, NULL);
+    for(int i=0;i<MAX_KASY;i++)
+        pthread_create(&kasy[i],NULL,kasa,(void*)(size_t)i);
 
-    for (int i = 0; i < 50; i++) {
-        Kibic* k = malloc(sizeof(Kibic));
-        k->id = i;
-        k->vip = (i % 20 == 0);
-        k->wiek = 20;
-        k->druzyna = i % 2;
-        k->bilety = 1 + rand() % 2;
-        k->sektor = rand() % SEKTORY;
-        pthread_create(&kibice[i], NULL, kibic, k);
-        usleep(50000);
+    for(int i=0;i<120;i++){
+        Kibic* k=malloc(sizeof(Kibic));
+        k->id=i;
+        k->vip=(rand()%1000<3);
+        k->wiek=rand()%40;
+        k->druzyna=rand()%2;
+        k->bilety=1+rand()%2;
+        k->sektor=rand()%SEKTORY;
+        pthread_create(&kibice[i],NULL,kibic,k);
     }
 
-    /* czekamy na wątki */
-for (int i = 0; i < MAX_KASY; i++)
-    pthread_join(kasy[i], NULL);
+    for(int i=0;i<120;i++) pthread_join(kibice[i],NULL);
+    for(int i=0;i<MAX_KASY;i++) pthread_join(kasy[i],NULL);
+    pthread_join(t_kier,NULL);
+    pthread_join(t_tech,NULL);
 
-for (int i = 0; i < 50; i++)
-    pthread_join(kibice[i], NULL);
+    printf("\n===== PODSUMOWANIE =====\n");
+    printf("Sprzedane: %d\nVIP: %d\nNormalni: %d\nDzieci: %d\nWeszli: %d\n",
+           sprzedane,stat_vip,stat_normalni,stat_dzieci,stat_wejsc);
 
-pthread_join(t_kier, NULL);
-pthread_join(t_tech, NULL);
-
-loguj("Koniec symulacji");
-return 0;
-
+    loguj("Koniec symulacji");
+    return 0;
 }
