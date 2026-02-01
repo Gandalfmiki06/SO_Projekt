d6feba242186b5349e34a789a15414c11b2a23e9
Mikołaj <163037224+Gandalfmiki06@users.noreply.github.com>
2026-02-01 15:58:38 +0100

    Update kierownik function for dynamic ticket management
    Refactor kierownik function to manage ticket sales dynamically based on queue length.

--- kierownik.c
+++ kierownik.c
@@ -1,20 +1,27 @@
 #include "hala.h"
 #include <unistd.h>
+#include <stdio.h>
 
 void* kierownik(void* arg)
 {
-    sleep(2);
-    stop_wejsc = 1;
-    loguj("Kierownik: stop wejsc");
+    while(sprzedane<K && !przerwanie){
+        pthread_mutex_lock(&mutex_kolejka);
+        int q=kibice_w_kolejce;
+        pthread_mutex_unlock(&mutex_kolejka);
 
-    sleep(2);
-    stop_wejsc = 0;
-    pthread_cond_broadcast(&cond_wejsc);
-    loguj("Kierownik: wznowienie wejsc");
+        int potrzebne=q/(K/10)+1;
+        if(potrzebne<2) potrzebne=2;
+        if(potrzebne>MAX_KASY) potrzebne=MAX_KASY;
 
-    sleep(5);
-    ewakuacja = 1;
+        pthread_mutex_lock(&mutex_kasy);
+        czynne_kasy=potrzebne;
+        pthread_mutex_unlock(&mutex_kasy);
+
+        sleep(1);
+    }
+
     loguj("Kierownik: ewakuacja");
-
+    ewakuacja=1;
+    pthread_cond_broadcast(&cond_wejsc);
     return NULL;
 }
