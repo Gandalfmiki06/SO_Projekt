9700067dd9847950217fc8b189d22083bc14881b
Mikołaj <163037224+Gandalfmiki06@users.noreply.github.com>
2026-02-01 15:58:58 +0100

    Enhance techniczny function with interruption handling
    Added check for interruption and mutex locking to ensure all sectors are empty before logging.

--- techniczny.c
+++ techniczny.c
@@ -3,9 +3,18 @@
 
 void* techniczny(void* arg)
 {
-    while (!ewakuacja)
-        sleep(1);
+    while(!ewakuacja && !przerwanie) sleep(1);
 
-    loguj("Techniczny: sektor pusty");
+    int puste;
+    do{
+        puste=1;
+        pthread_mutex_lock(&mutex_wejsc);
+        for(int i=0;i<SEKTORY;i++)
+            if(osoby_w_sektorze[i]>0) puste=0;
+        pthread_mutex_unlock(&mutex_wejsc);
+        sleep(1);
+    }while(!puste);
+
+    loguj("Techniczny: wszystkie sektory puste");
     return NULL;
 }
